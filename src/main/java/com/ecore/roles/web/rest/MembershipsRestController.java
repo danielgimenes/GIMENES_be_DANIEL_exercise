93e8a1f2976df857ef0b03c333c2ad545d8197aa
Daniel Gimenes <danielcgimenes@gmail.com>
2023-05-30 20:15:53 -0300

    Using stream API in controllers

--- src/main/java/com/ecore/roles/web/rest/MembershipsRestController.java
+++ src/main/java/com/ecore/roles/web/rest/MembershipsRestController.java
@@ -9,9 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static com.ecore.roles.web.dto.MembershipDto.fromModel;
 
@@ -40,16 +40,10 @@ public class MembershipsRestController implements MembershipsApi {
             produces = {"application/json"})
     public ResponseEntity<List<MembershipDto>> getMemberships(
             @NotNull @RequestParam UUID roleId) {
-
-        List<Membership> memberships = membershipsService.getMemberships(roleId);
-
-        List<MembershipDto> newMembershipDto = new ArrayList<>();
-
-        for (Membership membership : memberships) {
-            MembershipDto membershipDto = fromModel(membership);
-            newMembershipDto.add(membershipDto);
-        }
-
+        List<MembershipDto> newMembershipDto =
+                membershipsService.getMemberships(roleId).stream()
+                        .map(MembershipDto::fromModel)
+                        .collect(Collectors.toList());
         return ResponseEntity
                 .status(200)
                 .body(newMembershipDto);
--- src/main/java/com/ecore/roles/web/rest/RolesRestController.java
+++ src/main/java/com/ecore/roles/web/rest/RolesRestController.java
@@ -1,6 +1,5 @@
 package com.ecore.roles.web.rest;
 
-import com.ecore.roles.model.Role;
 import com.ecore.roles.service.RolesService;
 import com.ecore.roles.web.RolesApi;
 import com.ecore.roles.web.dto.RoleDto;
@@ -9,9 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static com.ecore.roles.web.dto.RoleDto.fromModel;
 
@@ -37,16 +36,9 @@ public class RolesRestController implements RolesApi {
     @GetMapping(
             produces = {"application/json"})
     public ResponseEntity<List<RoleDto>> getRoles() {
-
-        List<Role> getRoles = rolesService.getRoles();
-
-        List<RoleDto> roleDtoList = new ArrayList<>();
-
-        for (Role role : getRoles) {
-            RoleDto roleDto = fromModel(role);
-            roleDtoList.add(roleDto);
-        }
-
+        List<RoleDto> roleDtoList = rolesService.getRoles().stream()
+                .map(RoleDto::fromModel)
+                .collect(Collectors.toList());
         return ResponseEntity
                 .status(200)
                 .body(roleDtoList);
